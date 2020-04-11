0ab0bf1ff985fd0e67fc6839a5623a88f2b28885
kiorisyshen <kiorisyshen@gmail.com>
2020-04-11 15:12:59 +0900

    Add linear algebra header

--- src/PPM.hpp
+++ src/PPM.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <Eigen>
 #include "Frame.hpp"
+#include "LinearAlgebra.hpp"
 
 namespace LearnRT {
 class PPM {
@@ -9,7 +9,7 @@ class PPM {
     ~PPM();
 
    public:
-    static void OutFrame2Stream(const Frame<Eigen::Vector3d> &frame, std::ostream &out) {
+    static void OutFrame2Stream(const Frame<Vec3d> &frame, std::ostream &out) {
         const int image_width  = frame.getWidth();
         const int image_height = frame.getHeight();
 
@@ -17,7 +17,7 @@ class PPM {
             << image_width << ' ' << image_height << "\n255\n";
         for (int j = image_height - 1; j > -1; --j) {
             for (int i = 0; i < image_width; ++i) {
-                const Eigen::Vector3d &e = frame.at(j, i);
+                const Vec3d &e = frame.at(j, i);
                 out << static_cast<int>(255.999 * e[0]) << ' '
                     << static_cast<int>(255.999 * e[1]) << ' '
                     << static_cast<int>(255.999 * e[2]) << '\n';
--- src/RayTracer.hpp
+++ src/RayTracer.hpp
@@ -11,7 +11,7 @@ class RayTracer {
     RayTracer() {
     }
 
-    bool drawFrame(Frame<Eigen::Vector3d> &frame, const Camera &camera) {
+    bool drawFrame(Frame<Vec3d> &frame, const Camera &camera) {
         bool ret = true;
 
         const int image_width  = frame.getWidth();
@@ -19,10 +19,10 @@ class RayTracer {
         const double vwidth    = camera.getVWidth();
         const double vheight   = camera.getVHeight();
 
-        const Eigen::Vector3d origin(0, 0, 0);
-        const Eigen::Vector3d d_vertical(0, vheight, 0);
-        const Eigen::Vector3d d_horizontal(vwidth, 0, 0);
-        const Eigen::Vector3d LB_corner(-vwidth / 2.0, -vheight / 2.0, -1.0);
+        const Vec3d origin(0, 0, 0);
+        const Vec3d d_vertical(0, vheight, 0);
+        const Vec3d d_horizontal(vwidth, 0, 0);
+        const Vec3d LB_corner(-vwidth / 2.0, -vheight / 2.0, -1.0);
 
         for (int j = image_height - 1; j > -1; --j) {
             Logger::GetLogger().info("Scanlines remaining: {}", j);
@@ -41,10 +41,10 @@ class RayTracer {
     }
 
    protected:
-    Eigen::Vector3d calcRayColor(const Ray &r) {
-        Eigen::Vector3d unit_direction = r.direction().normalized();
-        auto t                         = 0.5 * (unit_direction(1) + 1.0);
-        return (1.0 - t) * Eigen::Vector3d(1.0, 1.0, 1.0) + t * Eigen::Vector3d(0.5, 0.7, 1.0);
+    Vec3d calcRayColor(const Ray &r) {
+        Vec3d unit_direction = r.direction().normalized();
+        auto t               = 0.5 * (unit_direction(1) + 1.0);
+        return (1.0 - t) * Vec3d(1.0, 1.0, 1.0) + t * Vec3d(0.5, 0.7, 1.0);
     }
 };
 
