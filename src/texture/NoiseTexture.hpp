8864e0d8b656bff3e89133f9c3689375f16e09f2
kiorisyshen <kiorisyshen@gmail.com>
2020-04-16 10:34:02 +0900

    Fix noise texture for different scene

--- src/texture/NoiseTexture.hpp
+++ src/texture/NoiseTexture.hpp
@@ -14,7 +14,8 @@ class NoiseTexture : public ITexture {
     virtual Vec3d value(double u, double v, const Vec3d &p) const {
         // return Vec3d(1, 1, 1) * 0.5 * (1.0 + noise.noise(scale * p));
         // return Vec3d(1, 1, 1) * noise.turb(scale * p);
-        return Vec3d(1, 1, 1) * 0.5 * (1.0 + sin(scale * p.z() + 10.0 * noise.turb(p)));
+        // return Vec3d(1, 1, 1) * 0.5 * (1.0 + sin(scale * p.z() + 10.0 * noise.turb(p)));
+        return Vec3d(1, 1, 1) * 0.5 * (1.0 + sin(scale * p.z() + 10.0 * noise.turb(scale * p)));
     }
 
    public:
